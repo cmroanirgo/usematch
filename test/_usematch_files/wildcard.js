1cd4ac20f2f752faa2d7c86628205e009a6aa48a
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-02-02 07:30:00 +1100

    added tests for deep keyvalue data

--- test/_usematch_files/wildcard.js
+++ test/_usematch_files/wildcard.js
@@ -8,5 +8,17 @@
 			{title:'first post'},
 			{title:'3rd post'},
 		]
+	},
+	all: {
+		keywords: {
+			key1: [
+				{title:'first post'},
+				{title:'2nd post'},
+				],
+			key2: [
+				{title:'first post'},
+				{title:'3rd post'},
+			]
+		}
 	}
 })
